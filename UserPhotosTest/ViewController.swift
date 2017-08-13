2b9aa9bd2713406795aa43990e1d8640834ba806
Vasiliy Vasilchenko <vasiji95@gmail.com>
2017-08-13 17:04:24 +0300

    Initial Commit

---
+++ UserPhotosTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserPhotosTest
+//
+//  Created by Vasiliy Vasilchenko on 8/13/17.
+//  Copyright © 2017 vasyl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
